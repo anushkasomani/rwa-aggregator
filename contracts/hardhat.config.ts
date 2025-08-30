810ad9a8a3e5c1182af7ba96625731d9186db4c4
jamiebones <jamiebones2000@yahoo.co.uk>
2025-08-30 14:29:23 +0100

    chore(git): remove node_modules from tracking and update .gitignore

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-foundry";
-// import "hardhat-contract-sizer"; // Temporarily disabled for testing
+// import "@nomicfoundation/hardhat-foundry"; // Temporarily disabled until foundry is properly installed
+import "hardhat-contract-sizer"; // Contract size reporting
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import * as dotenv from "dotenv";
@@ -63,8 +63,8 @@ const config: HardhatUserConfig = {
   contractSizer: {
     alphaSort: true,
     disambiguatePaths: false,
-    runOnCompile: true,
-    strict: true,
+  runOnCompile: false,
+  strict: false,
   },
   typechain: {
     outDir: "typechain-types",
